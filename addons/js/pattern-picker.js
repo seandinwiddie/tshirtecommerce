7ee1b24d7ec0ecd8fde330e5b530fd9d2f2c92ee
Sean Dinwiddie <seandinwiddie@gmail.com>
2017-10-18 14:14:40 -0700

    Update pattern-picker.js

--- addons/js/pattern-picker.js
+++ addons/js/pattern-picker.js
@@ -12,6 +12,10 @@ var rgb2hex = ( rgb ) =>
   }
   return "#" + hex( rgb[ 1 ] ) + hex( rgb[ 2 ] ) + hex( rgb[ 3 ] );
 }
+QUnit.test( "Pattern Chooser", ( assert ) =>
+{
+  assert.equal( rgb2hex( 'rgb(238, 87, 87)' ), "#EE5757" );
+} );
 /*  For selecting/locating and adjusting the SVG shapes  */
 var shapes = 'text,path,rect,line,circle,polygon,ellipse,polyline';
 var DataColorOfShapes = () =>
@@ -41,6 +45,14 @@ var FillOfShapesIs = ( selectedColor ) =>
 var Item = () =>
 {
   return design.item.get();
+}
+var ItemWidth = () =>
+{
+  return parseInt( jQuery( Item() ).css( 'width' ) );
+}
+var ItemHeight = () =>
+{
+  return parseInt( jQuery( Item() ).css( 'height' ) );
 }
 var ItemShapes = () =>
 {
@@ -75,15 +87,11 @@ var Container = () =>
       for syncing the active state of the design with the Color Picker */
 var SelectedColor = () =>
 {
-  if (
-    SlctdDdClr().attr( 'data-color' ).includes( 'url' )
-  )
+  if ( SlctdDdClr().attr( 'data-color' ).includes( 'url' ) )
   {
     return SlctdDdClr().attr( 'data-color' );
   }
-  else if (
-    SlctdDdClr().attr( 'data-color' ).includes( '#' )
-  )
+  else if ( SlctdDdClr().attr( 'data-color' ).includes( '#' ) )
   {
     return SlctdDdClr().attr( 'data-color' ).toUpperCase();
   }
@@ -105,24 +113,29 @@ var ChckSlctn = () =>
   console.log( "Item().find( DataColorOfShapesIs( SelectedColor() ) ):" );
   console.log( Item().find( DataColorOfShapesIs( SelectedColor() ) ) );
 }
-/*  Set a shape's fill and/or data-color attribute;
-      also sets the data-selected attribute.  */
-var SetFill = () =>
+var SlctShp = () =>
 {
   SlctdShp().removeAttr( 'data-selected' );
-  ChckSlctn();
+  // ChckSlctn();
   /*  Identify which shape is receiving the color or pattern  */
   Item().find( FillOfShapesIs( SelectedColor() ) )
     .attr( 'data-selected', true );
   Item().find( DataColorOfShapesIs( SelectedColor() ) )
     .attr( 'data-selected', true );
+}
+/*  Set a shape's fill and/or data-color attribute;
+      also sets the data-selected attribute.  */
+var SetFill = () =>
+{
+  SlctShp();
   /*  Find shapes with the data-color attribute set/defined  */
   if ( Item().find( '[data-selected]' ).length > 0 )
   {
     /*  Sync the dropdown-color to the shape  */
     var DataColor = () =>
     {
-      if (
+      if
+      (
         ! SlctdShp().attr( 'fill' ).includes( 'url' ) &&
         SlctdShp().attr( 'fill' ) == SelectedColor() 
       )
@@ -140,7 +153,8 @@ var SetFill = () =>
       .attr( 'data-color', DataColor() );
     /*  Sync the shape to the dropdown-color  */
     if ( ! ItemShapes().attr( 'fill' ).includes( 'url' ) )
-    {
+    {  
+      // MUST ADJUST FOR IF THE PATTERN IS SET
       Item().find( DataColorOfShapes() )
        .attr( 'fill', SelectedColor() )
        .attr( 'data-color', SelectedColor() );        
@@ -159,28 +173,37 @@ var ColorPickerColor = () =>
   var colorChangedTo = () =>
   {
     return jQuery( Container()[ 0 ] ).find( '.sp-thumb-active' )
-    .attr( 'title' );
+      .attr( 'title' ).toUpperCase();
   }
   SlctdDdClr().attr( 'data-color', colorChangedTo() );
-  console.log( 'colorChangedTo():' );
-  console.log( colorChangedTo() );
+  // MUST ADJUST FOR IF THE PATTERN IS SET... Might not be needed anymore...
+  //SlctdShp().attr( 'fill', colorChangedTo() );
+  SetFill();
+}
+// Needs to be combined with ColorPickerColor()
+var SetColor = ( color ) =>
+{
+  SlctdDdClr().attr( 'data-color', color );
+  SlctdShp().attr( 'fill', color );
   SetFill();
 }
 /*  Add Pattern Picker - the tabs for toggling to and from and it's panel  */
 jQuery( document ).on( 'select.item.design', ( event, e ) =>
 {
-  var whichDropdownColor = 0;
   setTimeout( () =>
   {
     /*  To add the tabs to the color picker dialog box  */
     jQuery( '.dropdown-color' ).each( ( index, element ) =>
     {
-      var whichColor = 'color-' + ++whichDropdownColor;
-      jQuery( element ).attr( 'data-which-color', whichColor );
+      jQuery( element ).attr( 'data-which-color', 'color-' + index );
       jQuery( element ).attr( 'onclick', 'PatternPicker( this )' );
+      jQuery( element ).on( 'move.spectrum', ( e, color ) =>
+      {
+        SetColor( color.toString().toUpperCase() );
+      } )
       jQuery( '.sp-choose' ).attr( 'onclick', 'ColorPickerColor();' );
     } );
-  }, 100 );
+  }, 200 );
 } );
 /* The Pattern Picker tabs and panel */
 var PatternPicker = ( tis ) =>
@@ -193,8 +216,8 @@ var PatternPicker = ( tis ) =>
   /*  Finally, actually adding the tabs  */
   if ( ! jQuery( Container() ).children().hasClass( 'tab' ) )
   {
-    jQuery( Container()[0] ).css( 'overflow', 'visible' );
-    jQuery( Container()[0] )
+    jQuery( Container()[ 0 ] ).css( 'overflow', 'visible' );
+    jQuery( Container()[ 0 ] )
       .append( '<div id="cp-tab" class="tab active-tab">Color</div>' )
       .append( '<div id="pp-tab" class="tab inactive-tab">Patten</div>' );
   }
@@ -205,16 +228,19 @@ var PatternPicker = ( tis ) =>
     jQuery( '#pp-tab, #cp-tab' ).detach();
     design.item.unselect();
   } );
-  /*  Switch/Toggle the Pattern Picker tab on, if there is a pattern set  */
-  if (
+  /*  Switch/Toggle the Pattern Picker tab on,
+        if there is a pattern set  */
+  if
+  (
     SlctdShp().attr( 'fill' ).includes( 'url' ) &&
     SlctdShp().attr( 'data-color' ) == SelectedColor()
-  ){
+  )
+  {
     jQuery( '#pp-tab' ).click();
-    jQuery( '.swatch:not(.selected-swatch)' )
+    /*jQuery( '.swatch:not(.selected-swatch)' )
       .addClass( 'swatch-disabled' )
       .removeClass( 'swatch-hover' )
-      .attr( 'onclick', '' );
+      .attr( 'onclick', '' );*/
     jQuery( '.remove-pattern' )
       .removeClass( 'disabled' )
       .attr( 'onclick', 'RemovePattern();ClosePicker();' );
@@ -223,14 +249,16 @@ var PatternPicker = ( tis ) =>
 /*  To clear the pattern from a design  */
 var RemovePattern = () =>
 {
-  if (
+  if
+  (
     SlctdShp().attr( 'data-color' ) == SelectedColor()
   )
   {
-    var idOfPatten = SlctdShp().attr( 'fill' );
-    idOfPatten = idOfPatten.replace( 'url(', '' );
-    idOfPatten = idOfPatten.replace( ')', '' );
-    jQuery( idOfPatten ).detach();
+    var IdOfPatten = () =>
+    {
+      return SlctdShp().attr( 'fill' ).replace( 'url(', '' ).replace( ')', '' );
+    }
+    jQuery( IdOfPatten() ).detach();
     SlctdShp().attr( 'fill', SelectedColor() );
   }
   jQuery( '.swatch' )
@@ -254,19 +282,26 @@ var TabSwitch = ( tis ) =>
       .removeClass( 'swatch-disabled' )
       .attr( 'onclick', 'SwatchClick( this )' );
     SetFill();
-    jQuery( '#cp-tab' ).removeClass( 'active-tab' ).addClass( 'inactive-tab' );
-    jQuery( '#pp-tab' ).addClass( 'active-tab' ).removeClass( 'inactive-tab' );
+    jQuery( '#cp-tab' ).removeClass( 'active-tab' )
+      .addClass( 'inactive-tab' );
+    jQuery( '#pp-tab' ).addClass( 'active-tab' )
+      .removeClass( 'inactive-tab' );
     if ( ! jQuery( '#pp' ).length >= 0 )
     {
-      jQuery( Container()[0] )
-        .append( '<div id="pp">' + jQuery( '#pttrns' ).html() + '</div>' );
+      jQuery( Container()[ 0 ] )
+        .append( '<div id="pp">' + jQuery( '#pttrns' )
+          .html() + '</div>' );
     }
   }
   else
   {
     RemovePattern();
-    jQuery( '#pp-tab' ).removeClass( 'active-tab' ).addClass( 'inactive-tab' );
-    jQuery( '#cp-tab' ).addClass( 'active-tab' ).removeClass( 'inactive-tab' );
+    jQuery( '#pp-tab' )
+      .removeClass( 'active-tab' )
+      .addClass( 'inactive-tab' );
+    jQuery( '#cp-tab' )
+      .addClass( 'active-tab' )
+      .removeClass( 'inactive-tab' );
     jQuery( '#pp' ).detach();
   }
 }
@@ -280,50 +315,83 @@ var ClosePicker = () =>
 /*  Swap in new pattern - refer to pattern-picker.php  */
 var SwatchClick = ( swatch ) =>
 {
-  /*  Create a pattern DOM element, to be inserted into the design element(s)  */
-  var pattern = jQuery( document.createElement( 'pattern' ) );
   /*  Grab the selected pattern  */
-  var swatch = jQuery( swatch );
+  var Swatch = () =>
+  {
+    return jQuery( swatch );
+  }
   jQuery( ".selected-swatch" ).removeClass( 'selected-swatch' );
-  swatch.addClass( 'selected-swatch' );
-  jQuery( '.swatch:not(.selected-swatch)' )
+  Swatch().addClass( 'selected-swatch' );
+  /*jQuery( '.swatch:not(.selected-swatch)' )
     .addClass( 'swatch-disabled' )
     .removeClass( 'swatch-hover' )
-    .attr( 'onclick', '' );
-  patternID = 'pattern-' + Item().attr( 'id' ) + '-' + swatch.attr( 'id' );
-  pattern.attr( 'id' , patternID );
-  pattern.addClass( swatch.attr( 'id' ) );
-  var patternHeight = swatch.find( 'svg' ).height();
-  var patternWidth = swatch.find( 'svg' ).width();
-  /*  Set the new pattern element's dimensions  */
-  pattern.attr( 'height', patternHeight );
-  pattern.attr( 'width', patternWidth );
-  var patternMaxDim = patternWidth >= patternHeight ? patternWidth : patternHeight;
-  var itemMaxDim = Item().width() >= Item().height() ?
-    Item().width() : Item().height();
-  var ratio = ( itemMaxDim / patternMaxDim ) * 1;
-  pattern.attr( 'patternUnits', 'userSpaceOnUse' );
-  pattern.attr( 'patternTransform', 'scale( '+ ratio +' )' );
-  /*  Insert the selected pattern into the new pattern element  */
-  var whichShape = 0;
-  jQuery( swatch.find( shapes ) ).each( ( index, element ) =>
+    .attr( 'onclick', '' );*/
+  var PatternID = () =>
   {
-    var whichPatternShape = 'shape-' + ++whichShape;
-    jQuery( element ).attr( 'data-which-shape', whichPatternShape );
-    /*  For removal from the color list, -../assets/js/design.js 4201-4207
-          as in not becoming one of the dropdown colors  */
-    jQuery( element ).attr( 'data-of-pattern', true );
-    pattern.append( jQuery( element ).clone() );
-  } );
-  pattern = pattern[0].outerHTML;
-  var defs = jQuery( document.createElement( 'defs' ) );
-  defs.innerHTML = pattern;
+    return 'pattern-' + Item().attr( 'id' ) + '-' + Swatch().attr( 'id' );
+  }
+  /*  Create a pattern DOM element,
+        to be inserted into the design element(s)  */
+  var Pattern = () =>
+  {
+    var pattern = jQuery( document.createElement( 'pattern' ) );
+    pattern.attr( 'id' , PatternID() );
+    pattern.addClass( Swatch().attr( 'id' ) );
+    /*  Insert the selected pattern into the new pattern element  */
+    jQuery( Swatch().find( shapes ) ).each( ( index, element ) =>
+    {
+      jQuery( element ).attr( 'data-which-shape', 'shape-' + index );
+      /*  For removal from the color list,
+            ( ../assets/js/design.js 4201 )
+            as in not becoming one of the dropdown colors  */
+      jQuery( element ).attr( 'data-of-pattern', true );
+      pattern.append( jQuery( element ).clone() );
+    } );
+    // PATTERN SIZING NEED TO BE TESTED MORE... x by x, y by x, y by y, x by x
+    var PatternHeight = () =>
+    {
+      //console.log( pattern.children().filter( ':lt(1)' ).width() );
+      return Swatch().find( 'svg' ).height();
+    }
+    var PatternWidth = () =>
+    {
+      return Swatch().find( 'svg' ).width();
+    }
+    var PatternMaxDim = () =>
+    {
+      return PatternWidth() >= PatternHeight() ? PatternWidth() : PatternHeight();
+    }
+    var ItemMaxDim = () =>
+    {
+      return ItemWidth() >= ItemHeight() ? ItemWidth() : ItemHeight();
+    }
+    var Ratio = () =>
+    {
+      return ( ItemMaxDim() / PatternMaxDim() ) * 1.0467;
+    }
+    /*  Set the new pattern element's dimensions  */
+    pattern.attr( 'height', PatternHeight() );
+    pattern.attr( 'width', PatternWidth() );
+    pattern.attr( 'patternUnits', 'userSpaceOnUse' );
+    pattern.attr( 'patternTransform', 'scale( ' + Ratio() + ' )' );
+    return pattern;
+  }
+  var PatternWrapper = () =>
+  {
+    return Pattern()[ 0 ].outerHTML;
+  }
+  var Defs = () =>
+  {
+    var defs = jQuery( document.createElement( 'defs' ) );
+    defs.innerHTML = PatternWrapper();
+    return defs;
+  }
   /*  Get the design elements that will receive the pattern,
         that had previous pattern set  */
   if ( ItemShapesDataColor() == SelectedColor() )
   {
     Item().find( DataColorOfShapesIs( SelectedColor() ) )
-      .attr( 'fill', 'url(#' + patternID + ' )' )
+      .attr( 'fill', 'url(#' + PatternID() + ' )' )
       .attr( 'data-color', SelectedColor() );
   }
   /*  Get the design elements that will receive the pattern,
@@ -332,25 +400,31 @@ var SwatchClick = ( swatch ) =>
   {
     Item().find( FillOfShapesIs( SelectedColor() ) )
       /*  Set their fill attribute  */
-      .attr( 'fill', 'url(#' + patternID + ' )' )
+      .attr( 'fill', 'url(#' + PatternID() + ' )' )
       .attr( 'data-color', SelectedColor() );
   }
   SetFill();
   /*  Insert the new pattern element into the design elements  */
   if ( Item().find( 'defs' ).length == 0 )
   {
-    Item().find( 'svg' ).append( defs );
+    Item().find( 'svg' ).append( Defs() );
   }
   if ( Item().find( 'defs' ).length != 0 )
   {
-    Item().find( '.' + swatch.attr( 'id' ) ).remove();
-    Item().find( 'defs' ).append( defs.innerHTML );
+    Item().find( '.' + Swatch().attr( 'id' ) ).remove();
+    Item().find( 'defs' ).append( Defs().innerHTML );
   }
-  var svg = Item().find( 'svg' )[0];
-  var svgContents = svg.innerHTML;
-  svg.innerHTML = svgContents;
-  setTimeout( () =>
+  var Svg = () =>
+  {
+    return Item().find( 'svg' )[ 0 ];
+  }
+  var svgContents = () =>
+  {
+    return Svg().innerHTML;
+  }
+  Svg().innerHTML = svgContents();
+  /*setTimeout( () =>
   {
     ClosePicker();
-  }, 250 );
+  }, 250 );*/
 }
